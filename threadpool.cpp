d1f3563fdadac9ec8d39f607998a2b145f77a6b6
Vladislav Kulikov <v.kulikov@partner.samsung.com>
2016-03-28 16:01:16 +0300

    Fix segmentation fault

--- threadpool.cpp
+++ threadpool.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <pthread.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -49,16 +50,19 @@ class ThreadPool {
 			return NULL;
 		if (high_in_process < 3 && !queues[P_HIGH].empty()) {
 			task = queues[P_HIGH].front();
+            cout << "Take task " << task << " with " << P_HIGH  << " priority" << endl;
 			queues[P_HIGH].pop();
 			high_in_process++;
 			return task;
 		}
 		if (!queues[P_NORMAL].empty()) {
 			task = queues[P_NORMAL].front();
+            cout << "Take task " << task << " with " << P_NORMAL  << " priority" << endl;
 			queues[P_NORMAL].pop();
 		}
 		else {
 			task = queues[P_LOW].front();
+            cout << "Take task " << task << " with " << P_LOW  << " priority" << endl;
 			queues[P_LOW].pop();
 		}
 		high_in_process = 0;
@@ -71,13 +75,15 @@ class ThreadPool {
 			pthread_mutex_lock(&(self->task_mutex));
 			while (self->state != STOPPING && (task = self->chooseTask()) == NULL)
 				pthread_cond_wait(&(self->task_cond), &(self->task_mutex));
-			if (self->state != STOPPING) {
+			if (self->state == STOPPING) {
 				pthread_mutex_unlock(&(self->task_mutex));
 				pthread_exit(NULL);
 			}
 			pthread_mutex_unlock(&(self->task_mutex));
+            cout << "Run task " << task << endl;
 			task->run();
-			//delete task;
+            cout << "End of  task " << task << endl;
+			delete task;
 		}
 		return NULL;
 	}
@@ -96,13 +102,19 @@ public:
 		if (state == STOPPING)
 			return false;
 		pthread_mutex_lock(&task_mutex);
+        cout << "Push " << t << " task with " << priority << " priority" << endl;
 		queues[priority].push(t);
 		pthread_cond_signal(&task_cond);
 		pthread_mutex_unlock(&task_mutex);
 		return true;
 	}
 	void Stop() {
+        if (state == STOPPING)
+            return;
 		state = STOPPING;
+		pthread_mutex_lock(&task_mutex);
+        pthread_cond_broadcast(&task_cond);
+		pthread_mutex_unlock(&task_mutex);
 		for (int i = 0; i < max_workers; i++) {
 			pthread_join(workers[i], NULL);
 		}
@@ -115,6 +127,43 @@ public:
 	}
 };
 
+class High : public Task {
+public:
+    virtual void run( void ) {
+        sleep(1);
+        cout << "High task" << endl;
+    }
+};
+
+class Normal : public Task {
+public:
+    virtual void run( void ) {
+        sleep(1);
+        cout << "Normal task" << endl;
+    }
+};
+
+class Low : public Task {
+public:
+    virtual void run( void ) {
+        sleep(1);
+        cout << "Low task" << endl;
+    }
+};
+
 int main( int argc, char ** argv ) {
+    ThreadPool tp(14);
+
+    for (int i = 0; i < 5; i++) {
+        tp.Enqueue(new High(), P_HIGH);
+        tp.Enqueue(new Normal(), P_NORMAL);
+        tp.Enqueue(new Low(), P_LOW);
+    }
+    tp.Enqueue(new High(), P_HIGH);
+    tp.Enqueue(new Normal(), P_NORMAL);
+    tp.Enqueue(new Normal(), P_NORMAL);
+    tp.Enqueue(new Low(), P_LOW);
+    sleep(2);
+    tp.Stop();
 	return 0;
 }
